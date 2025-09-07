cca34d5ec5f38f762c8162ee7ded736667581aa0
alhasanmolla <alhasannur99@gmail.com>
2025-09-07 19:37:52 +0600

    CICD run - 3

--- flask_app/app.py
+++ flask_app/app.py
@@ -61,8 +61,27 @@ def normalize_text(text):
     return text
 
 # ---------------------- MLflow & DagsHub Setup ----------------------
-mlflow.set_tracking_uri('https://dagshub.com/alhasannur99/Sentiment-analysis.mlflow')
-dagshub.init(repo_owner='alhasannur99', repo_name='Sentiment-analysis', mlflow=True)
+# mlflow.set_tracking_uri('https://dagshub.com/alhasannur99/Sentiment-analysis.mlflow')
+# dagshub.init(repo_owner='alhasannur99', repo_name='Sentiment-analysis', mlflow=True)
+
+
+# Below code block is for production use
+# -------------------------------------------------------------------------------------
+# Set up DagsHub credentials for MLflow tracking
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+
+dagshub_url = "https://dagshub.com"
+repo_owner = "alhasannur99"
+repo_name = "Sentiment-analysis"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+# -------------------------------------------------------------------------------------
+
 
 # ---------------------- Flask App Setup ----------------------
 app = Flask(__name__)
--- src/model/register_model.py
+++ src/model/register_model.py
@@ -16,25 +16,25 @@ warnings.filterwarnings("ignore")
 # Below code block is for production use
 # -------------------------------------------------------------------------------------
 # Set up DagsHub credentials for MLflow tracking
-# dagshub_token = os.getenv("CAPSTONE_TEST")
-# if not dagshub_token:
-#     raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
+dagshub_token = os.getenv("CAPSTONE_TEST")
+if not dagshub_token:
+    raise EnvironmentError("CAPSTONE_TEST environment variable is not set")
 
-# os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
-# os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
+os.environ["MLFLOW_TRACKING_USERNAME"] = dagshub_token
+os.environ["MLFLOW_TRACKING_PASSWORD"] = dagshub_token
 
-# dagshub_url = "https://dagshub.com"
-# repo_owner = "vikashdas770"
-# repo_name = "YT-Capstone-Project"
-# # Set up MLflow tracking URI
-# mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
+dagshub_url = "https://dagshub.com"
+repo_owner = "alhasannur99"
+repo_name = "Sentiment-analysis"
+# Set up MLflow tracking URI
+mlflow.set_tracking_uri(f'{dagshub_url}/{repo_owner}/{repo_name}.mlflow')
 # # -------------------------------------------------------------------------------------
 
 
 
-# ----------------- For local run -----------------
-mlflow.set_tracking_uri('https://dagshub.com/alhasannur99/Sentiment-analysis.mlflow')
-dagshub.init(repo_owner='alhasannur99', repo_name='Sentiment-analysis', mlflow=True)
+# # ----------------- For local run -----------------
+# mlflow.set_tracking_uri('https://dagshub.com/alhasannur99/Sentiment-analysis.mlflow')
+# dagshub.init(repo_owner='alhasannur99', repo_name='Sentiment-analysis', mlflow=True)
 # -------------------------------------------------
 
 class ModelRegister:
